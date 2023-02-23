66ec1c4ad4c70c30d0891e7940ba2668c64e18a7
Neslisah <neslisah.celek@outlook.com>
2023-02-23 11:45:17 +0300

    operators added

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -15,9 +15,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
     }
+    var text = binding.editTextNumber.text.toString()
     fun btnNumberClick(view: View){
+        if (newOp){
+            binding.editTextNumber.setText("")
+        }
+        newOp=false
         var btnSelect = view as Button
-        var btnClickValue = binding.editTextNumber.text.toString()
+        var btnClickValue = text
 
         when(btnSelect.id){
             binding.btn0.id -> {
@@ -50,13 +55,72 @@ class MainActivity : AppCompatActivity() {
             binding.btn9.id -> {
                 btnClickValue+="9"
             }
-            binding.btnminus.id -> {
-                btnClickValue+="-" + btnClickValue
+            binding.btncomma.id -> {
+                btnClickValue+="," + btnClickValue
             }
-            binding.btnplus.id -> {
-                btnClickValue+="+" + btnClickValue
+            binding.btndot.id -> {
+                btnClickValue+="." + btnClickValue
             }
         }
         binding.editTextNumber.setText(btnClickValue)
     }
+    var operator = "*"
+    var lastNumber = ""
+    var newOp = true
+
+    fun btnOpClick(view: View){
+        var btnSelect = view as Button
+        when(btnSelect.id){
+            binding.btnslash.id ->
+                operator="/"
+            binding.btnX.id ->
+                operator="x"
+            binding.btnpercent.id ->
+                operator="%"
+            binding.btnminus.id ->
+                operator="-"
+            binding.btnplus.id ->
+                operator="+"
+        }
+        lastNumber = text
+        newOp = true
+    }
+    fun btnEqualsClick(view: View){
+        var newNum = text
+        var resultNum:Double? = null
+        when(operator){
+            "/" ->
+                resultNum = lastNumber.toDouble() / newNum.toDouble()
+            "x" ->
+                resultNum = lastNumber.toDouble() * newNum.toDouble()
+            "%" ->
+                resultNum = lastNumber.toDouble() % newNum.toDouble()
+            "-" ->
+                resultNum = lastNumber.toDouble() - newNum.toDouble()
+            "+" ->
+                resultNum = lastNumber.toDouble() + newNum.toDouble()
+        }
+        binding.editTextNumber.setText(resultNum.toString())
+        newOp=true
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
